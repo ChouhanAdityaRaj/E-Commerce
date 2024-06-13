20dc977f38572f973422bbfc9d105d3e40e50a56
AdityaChouhanWD <ac6932680@gmail.com>
2024-06-13 17:55:54 +0530

    add error respone and try catch handlers AND fix minor bug

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (requestHadler) => {
+  return (req, res, next) => {
+    Promise
+        .resolve(requestHadler(req, res, next))
+        .catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
