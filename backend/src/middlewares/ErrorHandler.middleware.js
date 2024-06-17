4f94534dd248c94729f5b7de40747e87bf98b44e
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-06-17 16:22:50 +0530

    add default values

--- backend/src/middlewares/ErrorHandler.middleware.js
+++ backend/src/middlewares/ErrorHandler.middleware.js
@@ -1,9 +1,10 @@
 const ErrorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode;
+    const statusCode = err.statusCode || 500;
 
     const response = {
-      statusCode: err.statusCode,  
-      message: err.message
+      statusCode: statusCode,  
+      message: err.message || "Something went wrong",
+      errors: err.errors || []
     };
   
     res.status(statusCode).json(response);
