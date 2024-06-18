b9b5ff4a677aa2242e3697b9c8c5a6f700c8e2e0
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-06-18 17:08:04 +0530

    complete the process of logout user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -73,4 +73,30 @@ const login = asyncHandler(async (req, res) => {
     ));
 });
 
-export { signup, login };
+const logout = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $unset:{
+        refreshToken: 1
+      }
+    }
+  );
+
+  const options = {
+    httpOnly: true, 
+    secure: true
+  }
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(
+      200,
+      {},
+      "User logged out"
+    ));
+});
+
+export { signup, login, logout };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/user.controller.js";
+import { signup, login, logout } from "../controllers/user.controller.js";
 import {validate} from "../middlewares/validate.middleware.js";
 import {signupSchema, loginSchema} from "../validations/user.schema.js";
+import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/signup").post(validate(signupSchema), signup);
 router.route("/login").post(validate(loginSchema), login);
+
+// Secure Routes
+router.route("/logout").post(verifyJWT, logout);
 
 export default router;
