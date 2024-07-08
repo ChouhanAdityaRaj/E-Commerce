17e925f00f3b30aeaf5ae8eed20cca04252550f5
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-07-08 16:38:13 +0530

    Fix a bug when any error occurs all images stored in public/temp will automatically delete

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,8 +1,19 @@
+import fs from "fs";
+
 const asyncHandler = (requestHadler) => {
   return (req, res, next) => {
     Promise
         .resolve(requestHadler(req, res, next))
-        .catch((err) => next(err));
+        .catch((err) => {
+          if(req.files){
+            for (const fieldName in req.files) {
+              req.files[fieldName].forEach(img => {
+                fs.unlinkSync(img.path);
+              });
+            }
+          }
+          next(err)
+        });
   };
 };
 
