54214a1a32c017394d772710282f644f7071d230
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-07-10 18:34:24 +0530

    add get-address-by-id endpoint

--- backend/src/controllers/address.controller.js
+++ backend/src/controllers/address.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js";
 import { Address } from "../models/address.model.js";
+import mongoose from "mongoose";
 
 const addAddress = asyncHandler(async (req, res) => {
     const {mobileNumber, pinCode, state, city, address}  = req.body;
@@ -102,8 +103,66 @@ const deleteAddress = asyncHandler(async (req, res) => {
         ))
 });
 
+const getAddressByid = asyncHandler(async (req, res) => {
+    const { addressid } = req.params;
+
+    const address = await Address.findById(addressid);
+
+    if(!address){
+        throw new ApiError(404, "Address not exist");
+    }
+
+    if(address?.user.toString() !== req.user?._id.toString()){
+        throw new ApiError(401, "Only creator can see");
+    } 
+
+    const addressInfo = await Address.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(`${addressid}`)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+                pipeline: [
+                    {
+                        $project: {
+                            fullName: 1,
+                            email: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                user: {
+                    $first: "$user"
+                }
+            }
+        }
+    ]);
+
+    if(!addAddress){
+        throw new ApiError(500, "Problme while finding address")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            addressInfo[0],
+            "Address fetched successfully"
+        ));
+});
+
 export {
     addAddress,
     updateAddress,
-    deleteAddress
+    deleteAddress,
+    getAddressByid
 }
--- backend/src/routes/address.routes.js
+++ backend/src/routes/address.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { addAddress, updateAddress, deleteAddress } from "../controllers/address.controller.js";
+import { addAddress, updateAddress, deleteAddress, getAddressByid } from "../controllers/address.controller.js";
 import { addAddressSchema, updateAddressSchema } from "../validations/address.schema.js";
 
 const router = Router();
@@ -11,5 +11,6 @@ router.use(verifyJWT);
 router.route("/").post(validate(addAddressSchema), addAddress);
 router.route("/:addressid").patch(validate(updateAddressSchema), updateAddress);
 router.route("/:addressid").delete(deleteAddress);
+router.route("/:addressid").get(getAddressByid);
 
 export default router;
