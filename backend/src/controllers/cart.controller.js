e7d71d0ec2a28a0d600273d9bde9765f8b182206
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-07-14 15:54:27 +0530

    add get-cart-info endpoint

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { Cart } from "../models/cart.model.js";
 import { Product } from "../models/product.model.js";
+import mongoose from "mongoose";
 
 function isEqual(obj1, obj2) {
     return JSON.stringify(obj1) === JSON.stringify(obj2);
@@ -138,8 +139,82 @@ const removeCartItem = asyncHandler(async (req, res) => {
         ))
 });
 
+const getCartInfo = asyncHandler(async (req, res) => {
+    const { cartid } = req.params;
+
+    const cart = await Cart.findById(cartid);
+
+    if(!cart){
+        throw new ApiError(404, "Cart not exist");
+    }
+
+    if(cart.user.toString() !== req.user._id.toString()){
+        throw new ApiError(401, "Only creator can update");
+    }
+
+    const cartInfo = await Cart.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(`${cartid}`)
+            }
+        },
+        { 
+            $unwind: "$items" 
+        }, 
+        {
+            $lookup: {
+                from: "products",
+                localField: "items.product",
+                foreignField: "_id",
+                as: "items.product",
+                pipeline: [
+                    {
+                        $project: {
+                            productName: 1,
+                            productImage: 1,
+                            price: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {   
+            $unwind: "$items.product" 
+        },
+        { 
+            $group: {
+                _id: "$_id",
+                user: { $first: "$user" },
+                items: { 
+                  $push: { 
+                      product: "$items.product", 
+                      size: "$items.size", 
+                      quantity: "$items.quantity",
+                      _id: "$items._id"
+                    }
+                },
+                totalAmount:  {$first: "$totalAmount"},
+            }
+        }
+    ]); 
+
+    if(!cartInfo){
+        throw new ApiError(500, "Problem while fetching cart information")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200, 
+            cartInfo[0],
+            "Cart information fetched successfully"
+        ))
+})
+
+
 export {
     addToCart,
     updateCartItem,
-    removeCartItem
+    removeCartItem,
+    getCartInfo
 }
--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addToCart, updateCartItem, removeCartItem } from "../controllers/cart.controller.js";
+import { addToCart, updateCartItem, removeCartItem, getCartInfo } from "../controllers/cart.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { addToCartSchema, updatecartItemSchematSchema } from "../validations/cart.schema.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -11,5 +11,6 @@ router.use(verifyJWT);
 router.route("/p/:productid").post(validate(addToCartSchema), addToCart);
 router.route("/:cartid/i/:itemid").patch(validate(updatecartItemSchematSchema), updateCartItem);
 router.route("/:cartid/i/:itemid").delete(removeCartItem);
+router.route("/:cartid").get(getCartInfo)
 
 export default router;
