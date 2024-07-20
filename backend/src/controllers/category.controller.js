65f10476be09668344dcff2f7d0f3c360cf95192
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-07-20 09:17:09 +0530

    update the get-all-categories endpoint

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -6,64 +6,7 @@ import mongoose from "mongoose";
 
 
 const getAllCategories = asyncHandler(async (req, res) => {
-    const categories = await Category.aggregate([
-        {
-            $lookup: {
-                from: "products",
-                localField: "_id",
-                foreignField: "category",
-                as: "products",
-                pipeline: [
-                    {
-                        $lookup: {
-                            from: "reviews",
-                            localField: "_id",
-                            foreignField: "product",
-                            as: "reviews",
-                        }
-                    },
-                    {
-                        $addFields: {
-                            sumOfReviewStar:  {
-                                $reduce: {
-                                    input: "$reviews",
-                                    initialValue: 0,
-                                    in: { $add: ["$$value", "$$this.rating"] }
-                                }
-                            },
-                            totalReview: { $size: "$reviews" },
-                        }
-                    },
-                    {
-                        $project: {
-                            productName: 1,
-                            productImage: 1,
-                            price: 1,
-                            rating: {
-                                $cond: {
-                                    if: { $eq: ["$totalReview", 0] },
-                                    then: null,
-                                    else: { $divide: ["$sumOfReviewStar", "$totalReview"] }
-                                  }
-                            },
-                        }
-                    }
-                ]
-            }
-        },
-        {
-            $addFields: {
-                products: "$products"
-            }
-        },
-        {
-            $project: {
-                name: 1,
-                description: 1,
-                products: 1, 
-            }
-        }
-    ]);
+    const categories = await Category.find().select("-updatedAt -createdAt");
 
     if(!categories){
         throw new ApiError(500, "Problem while fetching categories");
