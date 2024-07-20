53bdb6e37f14552702b7b10b608eb16c6d766422
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-07-20 10:22:20 +0530

    add get-product-by-id endpoint

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -121,6 +121,91 @@ const searchProducts = asyncHandler(async (req, res) => {
         ));
 });
 
+const getProductById = asyncHandler(async (req, res) => {
+    const { productid } = req.params;
+
+    const product = await Product.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(`${productid}`)
+            }
+        },
+        {
+            $lookup: {
+                from: "reviews",
+                localField: "_id",
+                foreignField: "product",
+                as: "reviews",
+            }
+        },
+        {
+            $lookup: {
+                from: "categories",
+                localField: "category",
+                foreignField: "_id",
+                as: "category",
+                pipeline: [
+                    {
+                        $project: {
+                            name: 1,
+                            description: 1
+                        }
+                    }
+                ]
+            },
+        },
+        {
+            $addFields: {
+                sumOfReviewStar:  {
+                    $reduce: {
+                        input: "$reviews",
+                        initialValue: 0,
+                        in: { $add: ["$$value", "$$this.rating"] }
+                    }
+                },
+                totalReview: { $size: "$reviews" },
+                category: {
+                    $first: "$category"
+                }
+            }
+        },
+        {
+            $project: {
+                productName: 1,
+                productImage: 1,
+                otherProductImages: 1,
+                description: 1,
+                price: 1,
+                category: 1,
+                stock: 1,
+                createdAt: 1,
+                rating: {
+                    $cond: {
+                        if: { $eq: ["$totalReview", 0] },
+                        then: null,
+                        else: { $divide: ["$sumOfReviewStar", "$totalReview"] }
+                      }
+                }
+            }
+        }
+    ])
+
+    console.log(product);
+
+    if(!product){
+        throw new ApiError(404, "Product not exist");
+    }
+
+    return res 
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            product[0],
+            "Product fetched successfully"
+        ));
+})
+
 export {
-    searchProducts
+    searchProducts,
+    getProductById
 }
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { searchProducts } from "../controllers/product.controller.js";
+import { searchProducts, getProductById } from "../controllers/product.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
 router.route("/").get(searchProducts);
+router.route("/:productid").get(getProductById);
 
 export default router;
