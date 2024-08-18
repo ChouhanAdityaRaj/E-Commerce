acf004cb23236211ebfd9ea9954c68de6751b210
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-08-18 21:39:59 +0530

    add featue to handle  sending data throw api

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,31 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const useApi = function (urlPath, method = "get", data) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError("");
 
-const useApi = function(urlPath, method="get"){
+        const response = data ? await axios[method](urlPath, data) : await axios[method](urlPath)
 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    })();
+  }, []);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                setLoading(true);
-                setError("");
-
-                const response = await axios[method](urlPath);
-
-                setData(response.data);
-                setLoading(false);
-            } catch (error) {
-                setError(error)
-            }
-        })()
-    }, [])
-    
-    return [data, loading, error];
-}
+  return [data, loading, error];
+};
 
 export default useApi;
