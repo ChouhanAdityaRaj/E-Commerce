74d6e5bee2b3a127a540fb0c597d6b3c5f8c1966
ChouhanAdityaRaj <adityarajschouhan@gmail.com>
2024-09-06 14:59:25 +0530

    create header footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { SlMagnifier, SlBasket, SlUser } from "react-icons/sl"
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { SearchBar } from "../components";
 
 function Header() {
 
-  const [isOpen, setIsOpen] = useState(false);
   const authStatus = useSelector((state) => state.auth.status);
-
+  const [isOpen, setIsOpen] = useState(false);
   const [navBackground, setNavBackground] = useState(false);
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
 
   const handleScroll = () => {
     if (window.scrollY > 50) {
@@ -49,26 +50,27 @@ function Header() {
   ]
 
   return (
-    <nav className={`transition duration-500 ease-in-out w-full fixed z-10 bg-transparent text-white hover:bg-white hover:text-black hover:shadow-sm hover:shadow-white ${navBackground ? 'bg-white' : 'bg-transparent'}`}>
-      <div className="container mx-auto flex items-center py-2 px-4 justify-between">
+    <nav className={`transition   duration-300  ease-linear w-full fixed z-10 bg-transparent text-white ${isOpen ? "bg-white hover:text-black" : "lg:hover:bg-white" } md:hover:text-black lg:hover:shadow-sm hover:shadow-white ${navBackground ? 'bg-white' : 'bg-transparent'} ${isSearchBarOpen? "bg-white" : ""}`}>
+      <div className={` container mx-auto flex items-center py-2 px-4 justify-between  ${!isOpen ? "bg-transparent": ""} `}>
         {/* Logo */}
         <div className="flex items-center">
-          <Link to="/" className={`text-2xl mx-2 font-bold ${navBackground ? 'text-black' : ''}`}>Heritage Clothiers</Link>
+          <Link to="/" className={`text-2xl mx-2 font-bold ${navBackground || isSearchBarOpen || isOpen ? 'text-black' : ''}`}>Heritage Clothiers</Link>
         </div>
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex flex-grow justify-center space-x-7">
           {navigationMenu.map((item, i) => (
             item.activeStatus ? 
-            <NavLink key={i} to={item.path} className={({isActive}) => ` hover:text-gray-600 border-black ${navBackground ? 'text-black' : ''} ${isActive ? "font-semibold" : ""}`}>{item.name}</NavLink>
+            <NavLink key={i} to={item.path} className={({isActive}) => ` hover:text-gray-600 border-black ${navBackground || isSearchBarOpen ? 'text-black' : ''} ${isActive ? "font-semibold" : ""}`}>{item.name}</NavLink>
             :null
           ))}
         </div>
 
         {/* Mobile Burger Menu Button */}
         <div className="md:hidden flex items-center">
+        <button onClick={() => setIsSearchBarOpen(!isSearchBarOpen)} className={` mx-3 pt-1 ${navBackground || isSearchBarOpen || isOpen ? 'text-black' : ''}`}><SlMagnifier/></button>
           <button onClick={() => setIsOpen(!isOpen)} className="text-white focus:outline-none">
-            <svg className={`w-6 h-6 ${navBackground ? 'text-black' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg className={`w-6 h-6 ${navBackground || isSearchBarOpen || isOpen ? 'text-black' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
             </svg>
           </button>
@@ -76,18 +78,19 @@ function Header() {
  
         {/* Desktop Cart and Account Section */}
         <div className="hidden md:flex items-center space-x-4 mr-3">
-          <Link to="/search" className={`hover:text-gray-600 mx-3 pt-1 ${navBackground ? 'text-black' : ''}`}><SlMagnifier/></Link>
 
-          {!authStatus && (<Link to="/login" className={`hover:text-gray-600 ${navBackground ? 'text-black' : ''}`}>Login</Link>)}
-          {!authStatus && (<Link to="/signup" className={`hover:text-gray-600 ${navBackground ? 'text-black' : ''}`}>Sign Up</Link>)}
+        <button onClick={() => setIsSearchBarOpen(!isSearchBarOpen)} className={` mx-3 pt-1 ${navBackground || isSearchBarOpen || isOpen ? 'text-black' : ''}`}><SlMagnifier/></button>
 
-          {authStatus && (<Link to="/cart" className={`hover:text-gray-600 ${navBackground ? 'text-black' : ''}`}><SlBasket/></Link>)}
-          {authStatus && (<Link to="/account" className={`hover:text-gray-600 ${navBackground ? 'text-black' : ''}`}><SlUser/></Link>)}
+          {!authStatus && (<Link to="/login" className={`hover:text-gray-600 ${navBackground || isSearchBarOpen ? 'text-black' : ''}`}>Login</Link>)}
+          {!authStatus && (<Link to="/signup" className={`hover:text-gray-600 ${navBackground || isSearchBarOpen ? 'text-black' : ''}`}>Sign Up</Link>)}
+
+          {authStatus && (<Link to="/cart" className={`hover:text-gray-600 ${navBackground || isSearchBarOpen ? 'text-black' : ''}`}><SlBasket/></Link>)} 
+          {authStatus && (<Link to="/account" className={`hover:text-gray-600 ${navBackground || isSearchBarOpen ? 'text-black' : ''}`}><SlUser/></Link>)}
         </div>
       </div>
 
       {/* Mobile Menu */}
-      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}  bg-white text-black border-b-2 absolute w-full`}>
+      <div className={`md:hidden transition-all duration-200 ease-in-out transform ${isOpen ? ' opacity-100' : ' opacity-0'} ${isSearchBarOpen ? 'hidden' : 'block'} ${navBackground || isSearchBarOpen ? 'text-black' : ''} bg-white text-black  absolute w-full overflow-hidden`}>
         {navigationMenu.map((item, i) => (
           item.activeStatus ?
           <Link key={i} to={item.path} className="block px-4 py-2 hover:bg-slate-100 text-center text-2xl font-semibold">{item.name}</Link>
@@ -100,6 +103,10 @@ function Header() {
         {authStatus && (<Link to="/cart" className="block px-4 py-2 hover:bg-slate-100 text-center text-2xl font-semibold">Cart</Link>)}
         {authStatus && (<Link to="/account" className="block px-4 py-2 hover:bg-slate-100 text-center text-2xl font-semibold">Account</Link>)}
       </div>
+
+
+      {/* Search Dropdown */}
+      <SearchBar isSearchBarOpen={isSearchBarOpen}/>
     </nav>
   );
 }
